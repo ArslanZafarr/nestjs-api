dc1fb3e0d946076c036f04d9ef0f21e405e06c3c
Muhammad  Arslan <arslanzafarr71@gmail.com>
2024-05-19 11:06:30 +0500

    delete auth complete instance

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TasksModule } from './tasks/tasks.module';
 import { Task } from './tasks/entities/task.entity';
@@ -18,7 +17,7 @@ import { Task } from './tasks/entities/task.entity';
       entities: [Task],
       synchronize: true,
     }),
-    AuthModule,
+
     TasksModule,
   ],
   controllers: [AppController],
